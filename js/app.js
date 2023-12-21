a4edd1e29370d3a00934a5c3b2ef9389d53f1ef0
Mandy Blackmind <153823895+Blaackmind@users.noreply.github.com>
2023-12-21 20:26:54 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,36 @@
+let jogosAlugados = 0;
+
+function contarEExibirJogosAlugados() {
+    console.log(`Total de jogos alugados: ${jogosAlugados}`);
+}
+
+
+
+
 function alterarStatus(id) {
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
+    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
 
     if (imagem.classList.contains('dashboard__item__img--rented')) {
-        imagem.classList.remove('dashboard__item__img--rented');
-        botao.classList.remove('dashboard__item__button--return');
-        botao.textContent='Alugar';
-
+        if (confirm(`Você tem certeza que deseja devolver o jogo ${nomeJogo.textContent}?`)) {
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return');
+            botao.textContent = 'Alugar';
+            jogosAlugados--;
+        }
     } else {
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
-        botao.textContent='Devolver';
+        botao.textContent = 'Devolver';
+        jogosAlugados++;
     }
+
+    contarEExibirJogosAlugados();
 }
+document.addEventListener('DOMContentLoaded', function () {
+    jogosAlugados = this.document.querySelectorAll('.dashboard__item__img--rented').length;
+    console.log(`Número inicial de jogos alugados: ${jogosAlugados}`);
+    contarEExibirJogosAlugados();
+});
